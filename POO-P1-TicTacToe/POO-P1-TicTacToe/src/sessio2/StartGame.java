ff34774761d013b2a49f0d2b55609d217b8d8415
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-01-31 11:22:05 +0100

    Práctica 2

--- POO-P1-TicTacToe/POO-P1-TicTacToe/src/sessio2/StartGame.java
+++ POO-P1-TicTacToe/POO-P1-TicTacToe/src/sessio2/StartGame.java
@@ -55,7 +55,7 @@ public class StartGame {
 			
 			
 			//TODO: Show current board 
-			
+			 nb b bbbb 
 			
 			while(!gameEnded) {
 				//TODO: Interact with user and make move
---
+++ POO-P2-Fight/POO-P2-Fight/src/presentation/TestGame.java
@@ -0,0 +1,94 @@
+package presentation;
+
+import java.util.Random;
+
+import domain.Game;
+import domain.SurvivorGame;
+
+public class TestGame {
+
+	public static void main(String[] args) {
+		/*TODO: Complete this class to test the game*/
+		
+		//"Standard" game
+		//**************
+		
+		
+		//Create game
+		
+		
+		//Generate random stats for warrior
+		//Add warrior to game, use Game's provided function
+		
+		
+		//Generate random stats for mage
+		//Add mage to game
+		
+		//Generate random stats for archer
+		//Add archer to game
+		
+		//Show info, inform user beginning of standard game
+		
+		
+		//Simulate combat (DO NOT USE while(true))
+		while(!currentGame.hasGameEnded()) {
+			
+			//Select fighters randomly, they have to be alive
+			
+			//Simulate battle until it ends
+			
+			//Battle has ended
+		}
+		
+		//When combat ends, show all **available** characters' info
+		
+		
+		//Show winner
+		
+		
+		//This is the end of "standard" game
+		//**************
+		
+		
+		
+		//"Survivor" game
+		//**************
+		
+		
+		//Create survivor game
+		
+		//Generate random stats for warrior
+		//Add warrior to game, use Game's provided function
+				
+		//Generate random stats for mage
+		//Add mage to game
+				
+		//Generate random stats for archer
+		//Add archer to game
+		
+		//Show info, inform user beginning of survivor game
+		
+		
+		//Simulate combat (DO NOT USE while(true))
+		while(!currentGame.hasGameEnded()) {
+
+			//Select fighters randomly, they have to be alive
+			
+			//Simulate battle until it ends
+			
+			//Battle has ended
+		}
+		
+		//When combat ends, show all available characters' info
+		
+		
+		//Show winner
+		
+		
+		//This is the end of "survivor" game
+		//**************
+		
+
+	}
+
+}
