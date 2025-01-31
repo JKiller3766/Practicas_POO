0ef17c7e822498bc648d057586679e5a9d1cd4bc
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-01-31 12:00:35 +0100

    Fin de la clase

--- POO-P2-Fight/POO-P2-Fight/src/domain/Character.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Character.java
@@ -1,5 +1,51 @@
 package domain;
+import java.util.Random;
 
-public class Character {
-
+public abstract class Character {
+	
+	private String name;
+	private int health;
+	private int dodge;
+	private static final int MAX_HEALTH = 100;
+	
+	public Character(String name, int health) {
+		Random alea = new Random();	
+		
+		this.name = name;
+		if (health < MAX_HEALTH) {
+			this.health = health;
+		} else {
+			this.health = MAX_HEALTH;
+		} 
+		
+		dodge = alea.nextInt(30, 61);
+	}
+	
+	public abstract boolean attack(Character enemy);
+	
+	public boolean takeDamage(int damage) {
+		Random alea = new Random();	
+		int dodgeValue = alea.nextInt(0, 100);
+		
+		if (dodgeValue <= dodge) {
+			health -= damage;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
