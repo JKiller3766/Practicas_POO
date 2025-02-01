2e6fb84259c813791bfe295513a31353a706117c
ninjin780 <ninjin780@gmail.com>
2025-02-01 17:22:22 +0100

    mago y luchador

--- POO-P2-Fight/POO-P2-Fight/src/domain/Mage.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Mage.java
@@ -1,5 +1,31 @@
 package domain;
 
-public class Mage {
-
+public class Mage extends Character {
+	private int mana;
+	private int magicPower;
+	private static final int SPELLCOST = 10;
+	
+	public Mage(String name, int health, int mana, int magicPower) {
+		super(name, health);
+		this.mana = mana;
+		this.magicPower = magicPower;
+	}
+	
+	public boolean attack(Character enemy) {
+		if(mana < SPELLCOST) {
+			return false;
+		}
+		else {
+		mana -= SPELLCOST;
+		return takeDamage(magicPower);
+		}
+	}
+		
+		public String toString() {
+			return "MAGE " + super.toString();
+		}
+		
+		public String getCharactersClass() {
+			return "Class: MAGE";
+		}
 }
--- POO-P2-Fight/POO-P2-Fight/src/domain/Warrior.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Warrior.java
@@ -2,4 +2,21 @@ package domain;
 
 public class Warrior extends Character {
 	private int strengh;
+	
+	public Warrior(String name, int health, int strengh) {
+		super(name, health);
+		this.strengh = strengh;
+	}
+	
+	public boolean attack(Character enemy) {
+		return takeDamage(this.strengh);
+	}
+	
+	public String toString() {
+		return "WARRIOR " + super.toString();
+	}
+	
+	public String getCharactersClass() {
+		return "Class: WARRIOR";
+	}
 }
