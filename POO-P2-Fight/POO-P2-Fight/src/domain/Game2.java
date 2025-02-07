42e40246da51dbc672811d49079dda7af1356df2
ninjin780 <ninjin780@gmail.com>
2025-02-07 14:01:29 +0100

    game 2 edit

--- POO-P2-Fight/POO-P2-Fight/src/domain/Game2.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Game2.java
@@ -1,26 +1,44 @@
 package domain;
 
 public class Game2 {
-	protected Character [] caracters;
+	protected Character [] characters;
 	protected int nextAvailableSlot;
 	protected Character currentAttacker;
 	protected Character currentDefender;
 	protected Character winner;
 	
-	public Game2() {
-		
-	}
+	
 	
 	public boolean selectFighter(int x, int y) {
-		
+		if ( !( characters[x].isAlive() )  ||  !( characters[y].isAlive() ) ) {
+			return false;
+		}
+		else{
+			currentAttacker = characters[x];
+			currentDefender = characters[y];
+			return true;
+		}
 	}
 	
 	public boolean performAttack() {
-		
+		if (currentAttacker.attack(currentDefender)) {
+			if (hasBattleEnded()) {
+				checkAndSetWinner()
+			}
+		}
 	}
 	
 	public String charactersAndStatus() {
-		
+		String charactersInfo = "";
+		for(int idx = 0; idx<nextAvailableSlot;idx++) {
+			if( !( characters[idx].isAlive() ) ) {
+				if(characters[characters.length-1].isAlive()){
+				characters[idx] = characters[characters.length-1];
+				nextAvailableSlot--;
+				}
+			}
+			charactersInfo += "(" + idx + ")" + characters[idx].getCharactersClass() + characters[idx].toString() + characters[idx].;
+		}
 	}
 	
 	public String fightersStatus() {
@@ -28,34 +46,18 @@ public class Game2 {
 	}
 	
 	public String getWinnerInfo() {
-		
+		if(winner == null) {
+			return "";
+		}
+		else {
+			return winner.getNameAndClass();
+		}
 	}
 	
 	public boolean hasGameEnded() {
-		
-	}
-	
-	public boolean hasBattleEnded() {
-		
-	}
-	
-	protected void checkAndSetWinner() {
-		
-	}
-	
-	public boolean createWarrior(String x, int y, int z) {
-		
-	}
-	
-	public boolean createArcher(String x, int y, int z) {
-		
-	}
-
-	public boolean createMage(String x, int y, int z) {
-		
-	}
-	
-	private boolean addCharacter(Character x) {
-		
+		if(winner != null) {
+			return true;
+		}
+		return false;
 	}
 }
