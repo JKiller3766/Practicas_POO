2d449bdc5efb423e3d3ced58f2ce3ab0f482425c
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-02-07 11:30:41 +0100

    game

--- POO-P2-Fight/POO-P2-Fight/src/domain/Game.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Game.java
@@ -1,61 +1,59 @@
 package domain;
 
 public class Game {
-	protected Character [] caracters;
+	protected Character [] characters;
 	protected int nextAvailableSlot;
 	protected Character currentAttacker;
 	protected Character currentDefender;
 	protected Character winner;
 	
-	public Game() {
-		
-	}
-	
-	public boolean selectFighter(int x, int y) {
-		
-	}
-	
-	public boolean performAttack() {
-		
-	}
-	
-	public String charactersAndStatus() {
-		
-	}
-	
-	public String fightersStatus() {
-		
-	}
-	
-	public String getWinnerInfo() {
-		
-	}
-	
-	public boolean hasGameEnded() {
-		
+	public Game(int numberOfCharacters) {
+		characters = new Character[numberOfCharacters];
+		nextAvailableSlot = 0;
 	}
 	
 	public boolean hasBattleEnded() {
-		
+		if (currentDefender.isAlive() && currentAttacker.isAlive()) {
+			return false;
+		}
+		return true;
 	}
 	
 	protected void checkAndSetWinner() {
+		int aliveCharacters = 0;
 		
+		for (int i = 0; i < characters.length; i++) {
+			if (characters[i] != null)
+				aliveCharacters++;
+		}
+		if (aliveCharacters == 1) {
+			winner = currentAttacker;
+		}
+			
 	}
 	
-	public boolean createWarrior(String x, int y, int z) {
+	public boolean createWarrior(String name, int health, int strength) {
+		Character warrior = new Warrior(name, health, strength);
 		
+		return addCharacter(warrior);
 	}
 	
-	public boolean createArcher(String x, int y, int z) {
-		
+	public boolean createArcher(String name, int health, int accuracy, int arrowDamage) {
+		Character archer = new Archer(name, health, accuracy, arrowDamage);
+		return addCharacter(archer);
 	}
 
-	public boolean createMage(String x, int y, int z) {
-		
+	public boolean createMage(String name, int health, int mana, int magicPower) {
+		Character mage = new Mage(name, health, mana, magicPower);
+		return addCharacter(mage);
 	}
 	
-	private boolean addCharacter(Character x) {
-		
+	private boolean addCharacter(Character character) {
+		if (nextAvailableSlot < characters.length) {
+			characters[nextAvailableSlot] = character;
+			nextAvailableSlot++;
+			return true;
+		}
+		return false;
 	}
 }
