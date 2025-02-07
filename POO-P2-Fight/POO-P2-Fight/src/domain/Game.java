18cf8f25477c2c279cb512ff028de2b88910218e
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-02-07 10:42:01 +0100

    creacion game 2 2

--- POO-P2-Fight/POO-P2-Fight/src/domain/Game.java
+++ POO-P2-Fight/POO-P2-Fight/src/domain/Game.java
@@ -1,5 +1,61 @@
 package domain;
 
 public class Game {
+	protected Character [] caracters;
+	protected int nextAvailableSlot;
+	protected Character currentAttacker;
+	protected Character currentDefender;
+	protected Character winner;
+	
+	public Game() {
+		
+	}
+	
+	public boolean selectFighter(int x, int y) {
+		
+	}
+	
+	public boolean performAttack() {
+		
+	}
+	
+	public String charactersAndStatus() {
+		
+	}
+	
+	public String fightersStatus() {
+		
+	}
+	
+	public String getWinnerInfo() {
+		
+	}
+	
+	public boolean hasGameEnded() {
+		
+	}
+	
+	public boolean hasBattleEnded() {
+		
+	}
+	
+	protected void checkAndSetWinner() {
+		
+	}
+	
+	public boolean createWarrior(String x, int y, int z) {
+		
+	}
+	
+	public boolean createArcher(String x, int y, int z) {
+		
+	}
 
+	public boolean createMage(String x, int y, int z) {
+		
+	}
+	
+	private boolean addCharacter(Character x) {
+		
+	}
 }
