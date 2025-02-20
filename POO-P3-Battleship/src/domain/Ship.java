5be6ffb73338df5e0127a60e2a946f258a9a657c
ninjin780 <ninjin780@gmail.com>
2025-02-20 14:31:11 +0100

    ShipType acabado, Ship a punto de acabar

--- POO-P3-Battleship/src/domain/Ship.java
+++ POO-P3-Battleship/src/domain/Ship.java
@@ -1,5 +1,51 @@
 package domain;
 
-public class Ship {
+public class Ship implements Comparable {
+	private ShipType shipType;
+	private int id;
+	private int numHits;
+	private static int usedIds = 0;
+	
+	public Ship(ShipType ship) {
+		shipType = ship;
+		numHits = 0;
+		id = usedIds;
+		usedIds++;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getShipTypeId() {
+		return shipType.getId();
+	}
+	public String getShipTypeName() {
+		return shipType.getName();
+	}
+	public int getSize() {
+		return shipType.getSize();
+	}
+	public boolean hasSunk() {
+		if(numHits == getSize()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public void increasHits() {
+		numHits++;
+	}
+	public String toString() {
+		String ShipInfo = "";
+		ShipInfo += "Id: " + getId() + "| Ship Type: " + getShipTypeId() + "| Size: " + getSize();
+		return ShipInfo;
+	}
+	public boolean equals(Object ship) {
+		if (ship instanceof Ship && ( Ship(ship) ) ) {
+
+			
+		}
+	}
+	
 
 }
--- POO-P3-Battleship/src/domain/ShipType.java
+++ POO-P3-Battleship/src/domain/ShipType.java
@@ -23,9 +23,36 @@ public class ShipType {
 	}
 	
 	public static void createShipTypes() {
-		String [] id = new String[5];
+		String [] id = new String[5];	
 		String [] name = new String[5];
 		int [] size = new int[5];
+		for(int i = 0; i<id.length;i++) {
+			switch(i) {
+			case 0: size[i] = 2; id[i] = "DE"; name[i] = "DESTROYER";
+			break;
+			case 1: size[i] = 3; id[i] = "SU"; name[i] = "SUBMARINE";
+			break;
+			case 2: size[i] = 3; id[i] = "CR"; name[i] = "CRUISER";
+			break;
+			case 3: size[i] = 4; id[i] = "BA"; name[i] = "BATTLESHIP";
+			break;
+			default: size[i] = 5; id[i] = "CA"; name[i] = "CARRIER";
+			}
+		createShipTypes(id, name, size);
+		}
+	}
+	
+	public ShipType [] getAvailableShipTypes() {
+		return availableShipTypes;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getSize() {
+		return size;
 	}
 
 }
