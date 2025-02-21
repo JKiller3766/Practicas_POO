ffabc9893ec74ec05f8fbfe6d10c4805b6ad197b
ninjin780 <ninjin780@gmail.com>
2025-02-21 10:45:58 +0100

    Iboard, OccupiedCell acabados

--- POO-P3-Battleship/src/domain/OccupiedCell.java
+++ POO-P3-Battleship/src/domain/OccupiedCell.java
@@ -1,5 +1,27 @@
 package domain;
 
-public class OccupiedCell {
-
+public class OccupiedCell extends Cell {
+	private Ship ship;
+	
+	public OccupiedCell(Ship ship) {
+		this.ship = ship;
+	}
+	public boolean isEmpty() {
+		return false;
+	}
+	public boolean hasShipSunk() {
+		if(ship.hasSunk()) {
+			return true;
+		}
+		return false;
+	}
+	public Ship getShip() {
+		return ship;
+	}
+	public String getShipTypeName() {
+		return ship.getShipTypeName();
+	}
+	protected void increaseNumShots() {
+		ship.increasHits();
+	}
 }
